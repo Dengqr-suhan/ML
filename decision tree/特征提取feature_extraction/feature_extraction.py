d6ffd8f7d7166df9bd6a5d7693b113cee7f19a11
溯涵 S.rc <132651783+Dengqr-suhan@users.noreply.github.com>
2024-03-10 18:02:49 +0800

    Add files via upload

---
+++ decision tree/特征提取feature_extraction/feature_extraction.py
@@ -0,0 +1,90 @@
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import jieba
+def dict_demo():
+    """
+    对字典类型的数据进行特征抽取
+    :return: None
+    """
+    data = [{'city': '北京','temperature':100}, {'city': '上海','temperature':60}, {'city': '深圳','temperature':30}]
+    # 1、实例化一个转换器类
+    transfer = DictVectorizer(sparse=False)
+    # 2、调用fit_transform
+    data = transfer.fit_transform(data)
+    print("返回的结果:\n", data)
+    # 打印特征名字
+    print("特征名字：\n", transfer.get_feature_names_out())
+
+    return None
+
+def english_count_demo():
+    """
+    对文本进行特征抽取，countvetorizer
+    :return: None
+    """
+    data = ["life is short,i like python", 
+            "life is too long,i dislike python"]
+    transfer = CountVectorizer()
+    data = transfer.fit_transform(data)
+    names = transfer.get_feature_names_out()
+
+    print(names)
+    print(data.toarray())
+    print(data)
+
+def cut_word(text):
+    text = " ".join(list(jieba.cut(text)))
+    return text
+
+def Chinese_count_demo():
+    """
+    对中文进行特征抽取
+    :return: None
+    """
+    data = ["一种还是一种今天很残酷，明天更残酷，后天很美好，但绝对大部分是死在明天晚上，所以每个人不要放弃今天。",
+            "我们看到的从很远星系来的光是在几百万年之前发出的，这样当我们看到宇宙时，我们是在看它的过去。",
+            "如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的事物相联系。"]
+    # 将原始数据转换成分好词的形式
+    text_list = []
+    for sent in data:
+        text_list.append(cut_word(sent))
+    print(text_list)
+
+    # 1、实例化一个转换器类
+    # transfer = CountVectorizer(sparse=False)
+    transfer = CountVectorizer()
+    # 2、调用fit_transform
+    data = transfer.fit_transform(text_list)
+    print("文本特征抽取的结果：\n", data.toarray())
+    print("返回特征名字：\n", transfer.get_feature_names_out())
+    pass
+
+def text_chinese_tfidf_demo():
+    """
+    对中文进行特征抽取
+    :return: None
+    """
+    data = ["一种还是一种今天很残酷，明天更残酷，后天很美好，但绝对大部分是死在明天晚上，所以每个人不要放弃今天。",
+            "我们看到的从很远星系来的光是在几百万年之前发出的，这样当我们看到宇宙时，我们是在看它的过去。",
+            "如果只用一种方式了解某样事物，你就不会真正了解它。了解事物真正含义的秘密取决于如何将其与我们所了解的事物相联系。"]
+    # 将原始数据转换成分好词的形式
+    text_list = []
+    for sent in data:
+        text_list.append(cut_word(sent))
+    print(text_list)
+
+    # 1、实例化一个转换器类
+    # transfer = CountVectorizer(sparse=False)
+    transfer = TfidfVectorizer(stop_words=['一种', '不会', '不要'])
+    # 2、调用fit_transform
+    data = transfer.fit_transform(text_list)
+    print("文本特征抽取的结果：\n", data.toarray())
+    print("返回特征名字：\n", transfer.get_feature_names_out())
+
+    return None
+if __name__ == '__main__':
+    dict_demo()
+    english_count_demo()
+    Chinese_count_demo()
+    text_chinese_tfidf_demo()
